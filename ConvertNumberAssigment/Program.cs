dab30f0175bdbb7cf7e4ec924858e1b5a3c5833c
Dober09 <lebohanglehutjo@gmail.com>
2024-10-31 14:17:55 +0200

    added Units enums and  modified by adding method to convert and  digital legnth

--- ConvertNumberAssigment/Program.cs
+++ ConvertNumberAssigment/Program.cs
@@ -1,21 +1,88 @@
 ﻿
+using ConvertNumberAssigment;
+using Double = ConvertNumberAssigment.Double;
+using Single = ConvertNumberAssigment.Single;
 
 namespace ConvertNumberAssignment
 {
-    enum Digital
-    {
-        one,
-        two,
-        three,
-        four,
-    }
-
-    enum DoubleDigit
-    {
-
-    }
+   
     public class Program
     {
+        /// <summary>
+        /// the number of digit in the number
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>the legnth of the string</returns>
+        public static int DigitLength(string number)
+        {
+            return number.Length;
+        }
 
+        public static int ConvertToInt(char number)
+        {
+            if (number == null) return 0;
+            return Convert.ToInt32(number);
+        }
+        public static void ConvertToWord(string number ) {
+
+            string toWord = "";
+            
+            int digit = DigitLength(number);
+            if (digit == 3) { 
+            int oneNum = ConvertToInt(number[0]);
+            int twoNum = ConvertToInt(number[1]);
+            int threeNum = ConvertToInt(number[2]);
+
+                if (oneNum >= 0 && oneNum <= 10)
+                {
+                    toWord = $"{(Single)oneNum} {Triple.hundred}";  
+                }
+
+                if ( twoNum == 0)
+                {
+                    toWord += $" {(threeNum!=0 ?(Single)threeNum:"")}" ;
+                }
+                else if ( twoNum == 1) 
+                {
+                    toWord += $"{(threeNum==0?(Single.ten):(Double)threeNum-1)}";
+                }
+                else
+                {
+                    toWord += $"{(Teen)twoNum - 2}  {(threeNum==0?"":(Single)threeNum)}";
+                }
+               
+            
+            }
+           
+
+
+
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Convert numbers to spoken words (text):\nCan only needs to support 4 digits.\nEnter the digit : ");
+            string numberString = Console.ReadLine();
+            //bool Isint = int.TryParse(numberString, out int number);
+            //if (Isint)
+            //{
+            //    if (number >= 0 && number < 10)
+            //    {
+            //        Console.WriteLine((Single)number); 
+            //    }else if (number >= 11 &&  number < 20)
+            //    {
+            //        Console.WriteLine((Double)number-11); 
+            //    }
+
+            //}
+
+
+            Console.WriteLine($" number of digits {DigitLength(numberString)}  {numberString[1]}");
+                       
+
+   
+        }
     }
 }
---
+++ ConvertNumberAssigment/Units.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertNumberAssigment
+{
+
+    /// <summary>
+    /// This enum stores the number 1 to 9
+    /// </summary>
+   public enum Single
+   {
+        zero ,
+        one,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten
+   }
+
+    /// <summary>
+    /// This enum stores the number 11 to 19
+    /// </summary>
+    public enum Double
+    {
+        eleven,
+        twelve,
+        thirteen,
+        fourteen,
+        fifteen,
+        sixteen,
+        seventeen,
+        eighteen,
+        nineteen,
+    }
+
+    /// <summary>
+    /// This enum stores the number (tens) 10 to 90
+    /// </summary>
+    public enum Teen
+    {
+
+       
+       twenty,
+       thirty,
+       forty,
+       fifty,
+       sixty,
+       seventy,
+       eighty,
+       ninety,
+    }
+
+    /// <summary>
+    /// This enum stores the number 100 and 1000
+    /// </summary>
+    enum Triple
+    {
+        hundred,
+        thousand
+    }
+}
